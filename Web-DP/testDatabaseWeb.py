d17150db3ab14281cd8878bc510bff9346f50082
dandelionsforyou <imanaka1234@gmail.com>
2025-06-07 13:21:07 +0700

    Script-Google

--- Web-DP/testDatabaseWeb.py
+++ Web-DP/testDatabaseWeb.py
@@ -1,8 +1,8 @@
 import requests
 import json
 
-url = "https://script.google.com/macros/s/AKfycby8rKVhToeZtXW3A72rh_JWSX_4n9ZvTS-AJ7wVUEnzHohByqjUCjvgTt4tDgbNdHKzYA/exec"
-data = {"status": "success", "device": "2"} # attempt or success, 1 or 2
+url = "https://script.google.com/macros/s/AKfycbzISEfGEs0ZLjFJFd63BUlMY8to4g7NoUtjgXt48raUSGWKwuEjtNxpcvK9q7kh36YFVg/exec"
+data = {"status": "success", "device": "1"} # attempt or success, 1 or 2
 
 response = requests.post(url, json=data) # url
 print(response.text)
